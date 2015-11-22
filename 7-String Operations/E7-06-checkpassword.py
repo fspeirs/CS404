b8bbc786c3581f063375be80bfcf3d1ec03f709a
Fraser Speirs <fs@cedars.inverclyde.sch.uk>
2015-11-22 15:52:27 +0000

    Finished E7-06

--- 7-String Operations/E7-06-checkpassword.py
+++ 7-String Operations/E7-06-checkpassword.py
@@ -15,9 +15,13 @@
 #
 #	Recall that len(x) will give you the *number of characters* in the string x, so you should
 #	call randint(0, len(x)-1) to get a random number that you can use as an index into the string.
+#
+# 	Your function should print "OK" if the user enters the right character and "Wrong!" if they enter the wrong one.
 #-------------------------------------------
 
-# Define your function here. Call it compare_strings()
+# Complete the function:
+def check_password(input_password):
+	# code here
 
 
 
@@ -28,14 +32,7 @@ import unittest
 
 class MyTests(unittest.TestCase):
 	def testFunctionExists(self):
-		self.assertTrue('compare_strings' in globals(), "You have not implemented the method, or it is not named compare_strings()")
-
-	def testNormalData(self):
-		self.assertTrue(compare_strings("SHOUT", "shout"))
-		self.assertFalse(compare_strings("SHOUT", "SHOOT"))
-		#-------------------------------------------
-		# Write more unit tests here.
-		#-------------------------------------------
+		self.assertTrue('check_password' in globals(), "You have not implemented the method, or it is not named check_password()")
 
 				
 if __name__ == '__main__':

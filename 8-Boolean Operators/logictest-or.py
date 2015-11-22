aa2f07ea0e704ab7b10420fd896e3f80b9327bb2
Fraser Speirs <fs@cedars.inverclyde.sch.uk>
2015-11-22 15:52:47 +0000

    Added Unit 8 Quiz Programs

---
+++ 8-Boolean Operators/logictest-or.py
@@ -0,0 +1,33 @@
+#!/bin/python
+from random import *
+
+def rand_bool():
+	return random() < 0.5
+	
+
+# Program to post random logic questions and track scores.
+score = 0
+streak = 0
+longest_streak = 0
+
+for i in range(0,20):
+	x = rand_bool()
+	y = rand_bool()
+	answer = x or y
+	input = raw_input("\n{2}: What is {0} OR {1}? [T/F] ".format(x, y, i)).lower()
+	if input == "t":
+		input = True
+	else:
+		input = False
+		
+	if input == answer:
+		score+=1
+		streak+=1
+		if streak > longest_streak:
+			longest_streak = streak
+		print "Correct!\n"
+	else:
+		print "Sorry, the answer was {0}.\n".format(answer)
+		streak = 0
+	
+	print "Current score: {0}/20. Current streak: {1}, Longest streak: {2}\n".format(score, streak, longest_streak)

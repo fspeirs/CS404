dcb9c7dddb198f02531756f0ba4b6cae40f4e5f6
Fraser Speirs <fraser@speirs.org>
2016-06-19 09:07:09 +0100

    Added Swift version

---
+++ 12-Loops/E12-01-WriteLines.swift
@@ -0,0 +1,25 @@
+/*:
+ Topic 12: Unit Tests
+ Exercise 12-1
+
+ E12-1-WrtieLines.py 
+
+---
+
+# Instructions
+
+Write a function that takes one integer parameter and writes out lines of ten * characters.
+
+The number of lines should be controlled by the Parameter. If you get done, modify the program to change the length of the line based on the parameter.
+
+---
+*/
+
+func write_lines(length: l) {
+	// Write code here
+}
+
+write_lines(10)
+write_lines(5)
+write_lines(7)
+write_lines(4)
